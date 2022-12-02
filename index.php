23fa283859cec1d9a34a0f41a0f96e5f422a398c
MeiselaOndang <meyselaondang@gmail.com>
2022-12-02 20:20:13 +0800

    phpdasar1

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo "Hello World <br>";
+$nama = "meisela";
+$umur = 20;
+
+echo "Nama Saya <strong>$nama</strong>, Saya berumur  $umur tahun, <br>";
+
+$namaAdik = "Embul";
+$umurAdik = 2;
+
+echo "Nama Adik saya <strong>$namaAdik</strong>, Adik saya berusia $umurAdik tahun.<br>";
+
+$selisih = $umur - $umurAdik;
+
+echo "selisih usia saya dan adik saya adalah $selisih tahun.";
